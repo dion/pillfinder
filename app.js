d569f04cd31dd76559fa5f6220764b264c0735f1
Dion Yang <jellopops@gmail.com>
2016-01-06 14:56:54 -0800

    fixed streaming cutoff

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 var express = require('express'),
 	app = express(),
-//	bodyParser = require('body-parser'),
 	http = require('http');
 
-/*app.use(bodyParser.urlencoded({
-	extended: true
-}));*/
 app.get("/pills/:query", function (req, res) {
 	var query = req.params.query;
-	    body = '';
 
 	http.get("http://www.healthline.com/v2/druginputautocomplete?query=" + query + "&for=pill", function (response) {
 		res.header('Content-Type', 'application/json');
 		res.header('Access-Control-Allow-Origin', '*');
-		firstRun = false;
+		var body = '';
 
 		response.on("data", function(chunk) {
-//			console.log("BODY: " + chunk);
 			body += chunk;
+		});
 
+		response.on("end", function() {
 			res.end(body);
 		});
 
